c2756da53764630d0471ee2a2c1a34b8452008e4
sfatal1st <105299426+sfatal1st@users.noreply.github.com>
2022-11-15 16:51:06 +0300

    Add files via upload

--- client-state/auth/task.js
+++ client-state/auth/task.js
@@ -2,24 +2,26 @@ const welcome = document.querySelector(".welcome");
 const form = document.getElementById("signin__form");
 const textId = document.getElementById("user_id");
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const formData = new FormData(form);
-    const xhr = new XMLHttpRequest();
-    xhr.open("POST", "https://netology-slow-rest.herokuapp.com/auth.php");
-    xhr.send(formData); 
-    xhr.onload = () => {
-        const data = JSON.parse(xhr.response);
-        if (data.success) {
-            form.closest(".signin").classList.remove("signin_active");
-            if (localStorage.userId === data.user_id) {
+if (!localStorage.userId) {
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        const formData = new FormData(form);
+        const xhr = new XMLHttpRequest();
+        xhr.responseType = 'json';
+        xhr.open("POST", "https://netology-slow-rest.herokuapp.com/auth.php");
+        xhr.send(formData); 
+        xhr.onload = () => {
+            if (xhr.response.success) {
+                form.closest(".signin").classList.remove("signin_active");
+                form.reset();
                 welcome.classList.add("welcome_active");
-                textId.textContent = localStorage.userId;
-            } else {
-                localStorage.userId = data.user_id;
-                welcome.classList.add("welcome_active");
-                textId.textContent = data.user_id;
-            }
-        } else alert("Неверный логин/пароль");
-    };
-});
+                localStorage.userId = xhr.response.user_id;
+                textId.textContent = xhr.response.user_id;    
+            } else alert("Неверный логин/пароль");
+        };
+    });
+} else {
+    form.closest(".signin").classList.remove("signin_active");
+    welcome.classList.add("welcome_active");
+    textId.textContent = localStorage.userId;
+};
